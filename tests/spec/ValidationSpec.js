54ac59f864487326b2c2d215935cb79f010f9de9
Seb Martin <gee.sucka.foo@gmail.com>
2013-01-02 12:00:58 -0500

    Created core objects and implemented givenViewModel() and validateObservable[s]()

--- tests/spec/ValidationSpec.js
+++ tests/spec/ValidationSpec.js
@@ -1,5 +1,68 @@
 describe("A validation rule", function() {
-    it("should have unit tests", function() {
-        expect(true).toBeTruthy();
+	function createBasicViewModel() {
+		return new (function() {
+			this.firstName = ko.observable();
+			this.lastName = ko.observable();
+		})();
+	}
+	
+	describe("when specifying a view model", function() {
+		it("should keep a reference to the VM", function() {
+			var vm = createBasicViewModel();
+			
+			var vmCtx = tko.givenViewModel(vm);
+			
+			expect(vmCtx.viewModel).toBe(vm);
+		});
+	});
+    
+    describe("when specifying observable(s) to validate", function() {
+        it("should allow specifying a single observable using the singular method", function() {
+            var vm = createBasicViewModel();
+            
+            var vmCtx = tko.givenViewModel(vm);
+            var obCtx = vmCtx.validateObservable(function(vm) {
+                return vm.firstName;
+            });
+            
+            expect(obCtx.observables).toEqual([vm.firstName]);
+        });
+        
+        it("should only accept a single ko.observable from the singular method", function() {
+            var vm = createBasicViewModel();
+                    
+            function act() {
+                var vmCtx = tko.givenViewModel(vm);
+                var obCtx = vmCtx.validateObservable(function(vm) {
+                    return [vm.firstName, vm.lastName];
+                });
+            }
+            
+            expect(act).toThrow();
+        });
+        
+        it("should only accept a ko.observable objects", function() {
+            var vm = createBasicViewModel();
+            
+            function act() {
+                var vmCtx = tko.givenViewModel(vm);
+                var obCtx = vmCtx.validateObservable(function(vm) {
+                    return "not an observable";
+                });                
+            }
+            
+            expect(act).toThrow();
+        });
+        
+        it("should allow specifying multiple observables using the plural method", function() {
+            var vm = createBasicViewModel();
+            
+            var vmCtx = tko.givenViewModel(vm);
+            var obCtx = vmCtx.validateObservables(function(vm) {
+                return [vm.firstName, vm.lastName];
+            });
+            
+            expect(obCtx.observables).toEqual([vm.firstName, vm.lastName]);
+        });
     })
 });
--- validation.js
+++ validation.js
@@ -2,6 +2,59 @@ if (ko === undefined) {
     throw "The knockout JS library must be included before this validation library.";
 }
 var tko = (function() {
+    // Fallback on native implementation of Object.create if available
+    var objectCreate = (Object.create === 'function' ? Object.create : function(o) {
+        function F() {}
+        F.prototype = o;
+        return new F();
+    });
+    
+    var RootObject = {};
+    
+    function createObservableContext(vmCtx, observables) {
+        // wrap observables into an array if not one already
+        if ((observables instanceof Array) == false) {
+            observables = [observables];
+        }
+        
+        // verify that all members of the array are observables
+        ko.utils.arrayForEach(observables, function(ob) {
+            if (ko.isObservable(ob) == false) {
+                throw new Error("validateObservable accepts only a single KO observable.");
+            }    
+        });
+        
+        var obCtx = {
+            viewModelContext: vmCtx,
+            observables: observables
+        }
+        
+        return obCtx;
+    }
+    
+    function createViewModelContext(viewModel) {
+        var vmCtx = {
+            viewModel: viewModel,
+            
+            validateObservable: function(fn) {
+                var ob = fn(this.viewModel);
+                return createObservableContext(this, [ob]);
+            },
+
+            validateObservables: function(fn) {
+                var obArray = fn(this.viewModel);
+                return createObservableContext(this, obArray);
+            }
+        };
+        
+        return vmCtx;
+    }
+    
+    return {
+        givenViewModel: function(viewModel){
+            return createViewModelContext(viewModel);
+        }
+    }; 
 
 })();
 
